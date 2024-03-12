265652d81604cb6082184912841395e2fd6a18eb
MS Rahman <msrahman048gmail.com>
2024-03-12 15:19:33 +0600

    edit props

--- react/react_topics/props_react.php
+++ react/react_topics/props_react.php
@@ -10,7 +10,11 @@
 
 <div id="main_content"><!-- Start Main Content -->
 <h2>React Props<span><a href="https://www.w3schools.com/react/react_props.asp" target="_blank"> Details</a></span></h2>
-
+<div>
+Props are arguments passed into React components.
+   <br><br>
+Props are passed to components via HTML attributes.
+</div>
       
 
 </div><!-- End Main Content -->
